5205780c8018c34b44509e1b8ca24fc6c36d9e9d
jjsub <jsamesub@gmail.com>
2015-01-05 22:22:57 -0600

    portafolio v.2

--- home/home.js
+++ home/home.js
@@ -4,6 +4,11 @@
   var home = angular.module('jj');
 
   home.controller('HomeCtrl', ['$scope', function($scope){
-    $scope.title = 'Home jjsub';
+    //$scope.title = 'Home jjsub';
+    $('.navbar-brand, .name p ').hide().fadeIn(2000);
+    $('#content2').waypoint(function(direction){
+      $('.projects p');
+
+    },{offset:'20%'});
   }]);
 })();
